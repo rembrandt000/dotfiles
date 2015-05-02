a2657b3e94261323a4b81f363eaa42a31312d203
rembrandt000 <rembrandt.bluewing@gmail.com>
2015-05-02 09:32:01 +0900

    modified:   mklink_qf560.bat 	modified:   .bash_history

--- mklink_qf560.bat
+++ mklink_qf560.bat
@@ -2,5 +2,6 @@ mklink %HOMEPATH%"\_vimrc" %HOMEPATH%"\dropbox\soft\dotfiles\_vimrc"
 mklink %HOMEPATH%"\.bash_history" %HOMEPATH%"\dropbox\soft\dotfiles\.bash_history"
 mklink %HOMEPATH%"\.bash_profile" %HOMEPATH%"\dropbox\soft\dotfiles\.bash_profile"
 mklink %HOMEPATH%"\_vimperatorrc" %HOMEPATH%"\dropbox\soft\dotfiles\_vimperatorrc.rec"
-mklink %HOMEPATH%"\.Vromerc" %HOMEPATH%"\dropbox\soft\dotfiles\ %HOMEPATH%"\dropbox\soft\dotfiles\.Vromerc"
+mklink %HOMEPATH%"\.Vromerc" %HOMEPATH%"\dropbox\soft\dotfiles\.Vromerc"
+mklink %HOMEPATH%"\.gitconfig" %HOMEPATH%"\dropbox\soft\dotfiles\.gitconfig"
 exit 0
